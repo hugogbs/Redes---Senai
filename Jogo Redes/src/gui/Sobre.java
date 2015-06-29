ad632415f02a72ccd71d56ee48d3d4d828be8ee6
Hugo <hugo.silva@ccc.ufcg.edu.br>
2015-06-29 17:57:36 -0300

    .

--- Jogo Redes/src/gui/Sobre.java
+++ Jogo Redes/src/gui/Sobre.java
@@ -8,6 +8,8 @@ import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Sobre extends JPanel {
 
@@ -24,6 +26,11 @@ public class Sobre extends JPanel {
 		setLayout(null);
 
 		JButton btnNewButton = new JButton("Voltar");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				MainGame.setTela(new Home());
+			}
+		});
 		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 15));
 		btnNewButton.setBounds(389, 556, 114, 33);
 		add(btnNewButton);
@@ -35,8 +42,8 @@ public class Sobre extends JPanel {
 		txtrLenaLearning.setLineWrap(true);
 		txtrLenaLearning.setFont(new Font("Tahoma", Font.PLAIN, 22));
 		txtrLenaLearning.setTabSize(4);
-		txtrLenaLearning.setText("\tLENA - Learning Network (Rede de aprendizagem em tradu\u00E7\u00E3o livre) \u00E9 uma aplica\u00E7\u00E3o desenvolvida para fundamentar o Trabalho de Conclus\u00E3o de Curso em T\u00E9cnico em Manuten\u00E7\u00E3o e Suporte em Inform\u00E1tica no SENAI Campina Grande-PB.\r\n\t\r\n\tO jogo tem como p\u00FAblico alvo, estudantes interessados em exercitar e ampliar seus conhecimentos em Redes de Computadores.\r\n\r\n\tObrigado por jogar e divirta-se!\r\n\r\nDesenvolvedores:\r\n\tHugo Gabriel Bezerra da Silva\r\n\tJorge Henrique Gomes da Silva e Melo\r\n\r\nOrientadora:\r\n\tCamila Sarmento");
-		txtrLenaLearning.setBounds(56, 62, 836, 444);
+		txtrLenaLearning.setText("\tLeNe - Learning Network (Rede de aprendizagem em tradu\u00E7\u00E3o livre) \u00E9 uma aplica\u00E7\u00E3o desenvolvida para fundamentar o Trabalho de Conclus\u00E3o de Curso em T\u00E9cnico em Manuten\u00E7\u00E3o e Suporte em Inform\u00E1tica no SENAI Campina Grande-PB.\r\n\t\r\n\tO jogo tem como p\u00FAblico alvo, estudantes interessados em exercitar e ampliar seus conhecimentos em Redes de Computadores.\r\n\r\n\tObrigado por jogar e divirta-se!\r\n\r\nDesenvolvedores:\r\n\tHugo Gabriel Bezerra da Silva\r\n\tJorge Henrique Gomes da Silva e Melo\r\n\r\nOrientadora:\r\n\tCamila Sarmento");
+		txtrLenaLearning.setBounds(53, 42, 836, 483);
 		add(txtrLenaLearning);
 
 		JLabel fundo = new JLabel(new ImageIcon(
